c9af46248a29b480507f735d848ed9ca0e8af685
hello-hello1 <tuk86458@temple.edu>
2021-02-11 09:54:52 -0500

    Hello！！！

---
+++ app/src/main/java/edu/temple/cis_3515_assignment_01/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.cis_3515_assignment_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
